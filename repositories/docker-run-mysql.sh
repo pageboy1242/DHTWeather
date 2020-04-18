1dd64326ff2e9a2b54e11637bfe0c0fcd7d12853
Seann Hicks <pageboy@hotmail.com>
2020-04-18 16:06:37 -0600

    Add persistent volume for MySQL

--- repositories/docker-run-mysql.sh
+++ repositories/docker-run-mysql.sh
@@ -1,1 +1,1 @@
-docker run -p 127.0.0.1:3306:3306/tcp --name dht-mysql -e MYSQL_ROOT_PASSWORD=password -d dht-mysql
+docker run -p 127.0.0.1:3306:3306/tcp --name dht-mysql -e MYSQL_ROOT_PASSWORD=password -v $HOME/mysql-data:/var/lib/mysql -d dht-mysql
